b89b91d1b8a45ed6e93c0e1c6ba17ffb82dbe263
GLOBAL\pnarayan <pavan.narayana@tektronix.com>
2023-11-03 16:21:32 +0530

    Revert "Initial commit"
    This reverts commit a3e43f319ca24e40ee52f04e0071cfa4f95a34ba.

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export const NAME: string
-export const PATH: string
-export const EXECUTABLE: string
-export const DISC_EXECUTABLE: string
--- kic-debug/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use std::env;
-const VERSION: &str = env!("CARGO_PKG_VERSION");
-pub mod command;
-pub mod debugger;
-pub mod error;
-pub mod resources;
